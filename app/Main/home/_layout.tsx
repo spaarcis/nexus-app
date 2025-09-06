9a60037d15f6390052756be6d8851b825888c4f6
SamyaRoy999 <samyaroy999@gmail.com>
2025-09-06 18:19:49 +0600

    change code home page btn bar

--- app/Main/home/_layout.tsx
+++ app/Main/home/_layout.tsx
@@ -1,14 +1,13 @@
 
-import { IconBookings, IconBookingsActive, IconExplore, IconExploreActive, IconHome, IconHomeActive, IconProfiles, IconProfilesActive, IconTabBarCard } from "@/Icons/Icons";
+import { IconBookings, IconBookingsActive, IconExplore, IconExploreActive, IconHome, IconHomeActive, IconProfiles, IconProfilesActive } from "@/Icons/Icons";
 import tw from "@/lib/tailwind";
 import { _Width } from "@/utils/utils";
 import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
 import { useLinkBuilder } from "@react-navigation/native";
 import { BlurView } from "expo-blur";
-import { LinearGradient } from "expo-linear-gradient";
-import { router, Tabs } from "expo-router";
-import React, { useEffect, useRef, useState } from "react";
-import { Animated, Dimensions, Modal, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Tabs } from "expo-router";
+import React, { useRef, useState } from "react";
+import { Animated, Dimensions, StyleSheet, TouchableOpacity, View } from "react-native";
 import { SvgXml } from "react-native-svg";
 
 const icons = {
@@ -44,54 +43,55 @@ function CustomTabBar({ state, descriptors, navigation }: BottomTabBarProps) {
         <View
             style={tw`flex-row absolute bottom-10 w-full justify-center items-center  bg-transparent `}
         >
-            {/* <SvgXml xml={IconTabBarCard} /> */}
-            <View style={tw`flex-row bg-[#312c44] opacity-70 gap-5 p-7 w-80 rounded-full `}>
-                {state.routes.map((route, index) => {
-                    const { options } = descriptors[route.key];
-                    const isFocused = state.index === index;
+            <View>
+                <BlurView style={tw`flex-row justify-between w-80 p-5 rounded-full overflow-hidden gap-6`} intensity={100} tint="dark">
+                    {state.routes.map((route, index) => {
+                        const { options } = descriptors[route.key];
+                        const isFocused = state.index === index;
 
-                    const onPress = () => {
-                        const event = navigation.emit({
-                            type: "tabPress",
-                            target: route.key,
-                            canPreventDefault: true,
-                        });
+                        const onPress = () => {
+                            const event = navigation.emit({
+                                type: "tabPress",
+                                target: route.key,
+                                canPreventDefault: true,
+                            });
 
-                        if (!isFocused && !event.defaultPrevented) {
-                            navigation.navigate(route.name);
+                            if (!isFocused && !event.defaultPrevented) {
+                                navigation.navigate(route.name);
+                            }
+                        };
+
+                        const onLongPress = () => {
+                            navigation.emit({
+                                type: "tabLongPress",
+                                target: route.key,
+                            });
+                        };
+
+                        if (route.name === "add") {
+                            return (
+                                <View key="add"  />
+                            );
                         }
-                    };
 
-                    const onLongPress = () => {
-                        navigation.emit({
-                            type: "tabLongPress",
-                            target: route.key,
-                        });
-                    };
+                        const iconSet = icons[route.name];
 
-                    if (route.name === "add") {
                         return (
-                            <View key="add" style={tw`flex-1`} />
+                            <TouchableOpacity
+                                key={route.name}
+                                accessibilityState={isFocused ? { selected: true } : {}}
+                                accessibilityLabel={options.tabBarAccessibilityLabel}
+                                testID={options.tabBarButtonTestID}
+                                href={buildHref(route.name)}
+                                onPress={onPress}
+                                onLongPress={onLongPress}
+                                style={tw` flex-row  justify-center items-center ${isFocused ? "bg-black  p-2 rounded-full " : ""}`}
+                            >
+                                <SvgXml xml={isFocused ? iconSet.active : iconSet.inactive} />
+                            </TouchableOpacity>
                         );
-                    }
-
-                    const iconSet = icons[route.name];
-
-                    return (
-                        <TouchableOpacity
-                            key={route.name}
-                            accessibilityState={isFocused ? { selected: true } : {}}
-                            accessibilityLabel={options.tabBarAccessibilityLabel}
-                            testID={options.tabBarButtonTestID}
-                            href={buildHref(route.name)}
-                            onPress={onPress}
-                            onLongPress={onLongPress}
-                            style={tw`flex-1 relative justify-center items-center ${isFocused ? "bg-black  p-2 rounded-full " : ""}`}
-                        >
-                            <SvgXml xml={isFocused ? iconSet.active : iconSet.inactive} />
-                        </TouchableOpacity>
-                    );
-                })}
+                    })}
+                </BlurView>
             </View>
         </View>
     );
--- app/Main/home/home.tsx
+++ app/Main/home/home.tsx
@@ -16,7 +16,7 @@ const Home = () => {
     const navigation = useNavigation();
 
     return (
-        <View style={tw` flex-1`}>  
+        <View style={tw` flex-1`}>
             <ImageBackground
                 source={ImgGradint}
                 resizeMode="cover"
@@ -50,7 +50,7 @@ const Home = () => {
                             <TouchableOpacity onPress={() => router.push("/(allPages)/notifications")}>
                                 <SvgXml xml={IconNotification} />
                             </TouchableOpacity>
-                            <TouchableOpacity onPress={()=> router.push("/Main/Home/Profile")}>
+                            <TouchableOpacity onPress={() => router.push("/Main/Home/Profile")}>
                                 <Image source={profileImg} style={tw`w-11 h-11`} />
                             </TouchableOpacity>
                         </View>
@@ -98,32 +98,40 @@ const Home = () => {
                 </Text>
                 {/* Your Next Station */}
                 <View style={tw`mb-4`}>
-                    <BlurView style={tw` p-5 border  rounded-3xl overflow-hidden flex-row items-center gap-4`} intensity={10} tint="light">
-                        <Image source={nextStation} style={[tw`h-20 w-20 rounded-2xl`, {
+                    <TouchableOpacity onPress={() => router.push({
+                        pathname: "/details/BookingsDetails/[id]",
+                        params: {
+                            id: 4,
+                            status: "Upcoming"
+                        },
+                    })} style={tw`mb-4`}>
+                        <BlurView style={tw` p-5 border  rounded-3xl overflow-hidden flex-row items-center gap-4`} intensity={10} tint="light">
+                            <Image source={nextStation} style={[tw`h-20 w-20 rounded-2xl`, {
 
-                        }]}></Image>
-                        <View style={tw`flex-1 items-start  justify-center`}>
-                            <Text style={tw`text-white font-bold text-lg`}>
-                                Mumba Esport House
-                            </Text>
+                            }]}></Image>
+                            <View style={tw`flex-1 items-start  justify-center`}>
+                                <Text style={tw`text-white font-bold text-lg`}>
+                                    Mumba Esport House
+                                </Text>
 
-                            {/* Date and Time */}
-                            <View style={tw`flex-row items-center mt-2 gap-2`}>
-                                <SvgXml xml={IconCleander} />
-                                <Text style={tw`text-white ml-2`}>9 June, 2025</Text>
-                                <SvgXml xml={IconTime} />
-                                <Text style={tw`text-white ml-1`}>10:00 AM</Text>
+                                {/* Date and Time */}
+                                <View style={tw`flex-row items-center mt-2 gap-2`}>
+                                    <SvgXml xml={IconCleander} />
+                                    <Text style={tw`text-white ml-2`}>9 June, 2025</Text>
+                                    <SvgXml xml={IconTime} />
+                                    <Text style={tw`text-white ml-1`}>10:00 AM</Text>
+                                </View>
+
+                                {/* Duration and Location */}
+                                <View style={tw`flex-row items-center mt-2 gap-2`}>
+                                    <SvgXml xml={Iconhoure} />
+                                    <Text style={tw`text-white ml-2`}>2 - Hour</Text>
+                                    <SvgXml xml={IconLoction} />
+                                    <Text style={tw`text-white ml-1`}>New York, USA</Text>
+                                </View>
                             </View>
-
-                            {/* Duration and Location */}
-                            <View style={tw`flex-row items-center mt-2 gap-2`}>
-                                <SvgXml xml={Iconhoure} />
-                                <Text style={tw`text-white ml-2`}>2 - Hour</Text>
-                                <SvgXml xml={IconLoction} />
-                                <Text style={tw`text-white ml-1`}>New York, USA</Text>
-                            </View>
-                        </View>
-                    </BlurView>
+                        </BlurView>
+                    </TouchableOpacity>
                 </View>
                 {/* Carousel Newly Added */}
                 <View style={tw`flex-row items-center justify-between pb-5`}>
